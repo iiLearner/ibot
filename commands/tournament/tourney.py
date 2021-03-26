1069321953f1a55c170567e468cfe2b47d7c3236
iiLearner <ilearner.432@gmail.com>
2021-03-26 09:54:20 +0100

    No reaction is author is imuted

--- commands/tournament/tourney.py
+++ commands/tournament/tourney.py
@@ -13,7 +13,7 @@ async def cancel_tourney(ctx):
     mycursor.execute("SELECT * FROM tournaments WHERE userid = " + str(ctx.message.author.id) + " AND status = 1")
     result = mycursor.fetchone()
     if result is None:
-        await ctx.message.channel.send("**[ERROR]** You don't have a tourney! Create one with `?create_tourney`!")
+        await ctx.message.channel.send("**[ERROR]** You don't have a tourney! Create one with `icreate_tourney`!")
         return
     else:
         try:
@@ -36,61 +36,67 @@ async def cancel_tourney(ctx):
 @client.command()
 @commands.cooldown(1, 30, commands.BucketType.user)
 async def create_tourney(ctx, *, name=None):
+
     if name is None:
         await ctx.message.channel.send("**Usage:** icreate_tourney `<name>`")
         return
 
-    perms = ctx.message.author.guild_permissions
-    if not perms.administrator:
-        await ctx.message.channel.send("**[ERROR]** You must be a server admin in order to create a tournament!")
-        return
-    name = name.replace("<", "").replace(">", "")
+    try:
+        perms = ctx.message.author.guild_permissions
+        if not perms.administrator:
+            await sendError("You must be a server admin in order to create a tournament!",  "", ctx)
+            return
 
-    con = await dbConnect()
-    mycursor = con.cursor()
-    mycursor.execute("SELECT * FROM tournaments WHERE userid = " + str(ctx.message.author.id) + " and status = 1")
-    result = mycursor.fetchone()
-    if result is not None:
-        await ctx.message.channel.send(
-            "**[ERROR]** You already have an ongoing tournament! cancel it first using `?cancel_tourney`!")
-        return
+        myperms = ctx.message.channel.guild.me.guild_permissions
+        if not myperms.manage_channels:
+            await sendError("I need manage channel permission in order to do this!", "", ctx)
+            return
 
-    await ctx.trigger_typing()
-    myperms = ctx.message.channel.guild.me.guild_permissions
-    if not myperms.manage_messages:
-        await sendError("I need manage channel permission in order to do this!", "", ctx)
-        return
+        name = name.replace("<", "").replace(">", "")
 
-    category = await ctx.message.channel.guild.me.guild.create_category(name)
-    overwrites = {
-        ctx.message.channel.guild.me.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        ctx.message.channel.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True),
-        ctx.message.author: discord.PermissionOverwrite(read_messages=True, send_messages=True)
-    }
-    logs = await ctx.message.channel.guild.me.guild.create_text_channel("┌📈𝗟𝗢𝗚𝗦", overwrites=overwrites, category=category)
-    alerts = await ctx.message.channel.guild.me.guild.create_text_channel("└👥𝗔𝗟𝗘𝗥𝗧𝗦", overwrites=overwrites, category=category)
+        con = await dbConnect()
+        mycursor = con.cursor()
+        mycursor.execute("SELECT * FROM tournaments WHERE userid = " + str(ctx.message.author.id) + " and status = 1")
+        result = mycursor.fetchone()
+        if result is not None:
+            await ctx.message.channel.send(
+                "**[ERROR]** You already have an ongoing tournament! cancel it first using `icancel_tourney`!")
+            return
 
-    av_commands = "Here's the list of available commands that can be used in {0} or {1} by organizers!\n`closeconnection name` - Close the connection for a" \
-                  "user.\n**Example:** `closeconnection iLearner`\n\n`processlist name` - Get the current list of running processes from task manager for a " \
-                  "user.\n**Example:**`processlist iLearner`\n\nHope you enjoy your time!".format(logs.mention,
-                                                                                                  alerts.mention)
-    em = discord.Embed(title='Available commands', description=av_commands, colour=0x2ecc71)
-    em.set_author(name='', icon_url=ctx.message.author.avatar_url)
-    em.set_footer(text=name, icon_url=ctx.message.channel.guild.me.avatar_url)
-    await logs.send(embed=em)
-    await alerts.send(embed=em)
+        await ctx.trigger_typing()
 
-    myemoji = client.get_emoji(787237116630532106)
-    chemoji = client.get_emoji(788129603876421632)
-    msg = '┌─────┈{0}┈────┐\n{6}Tournament created!{7}\n└─────┈{1}┈────┘\n\nTournament has been created!\n\nTournament name: {2}\nTournament organizer: {3}\nLogs channel: {4}\nAlerts channel: {5}\n\nPlease add members to your tournament with the command `itourney_role @role`\n\nSend private keys to all members before the tournament with the command `isendkeys`'.format(
-        myemoji, myemoji, name, ctx.message.author.mention, logs.mention, alerts.mention, chemoji, chemoji)
-    em = discord.Embed(title='', description=msg, colour=0x9b59b6)
-    em.set_author(name='', icon_url=ctx.message.author.avatar_url)
-    em.set_footer(text="Tournament created!", icon_url=ctx.message.channel.guild.me.avatar_url)
-    await ctx.message.channel.send(embed=em)
+        category = await ctx.message.channel.guild.me.guild.create_category(name)
+        overwrites = {
+            ctx.message.channel.guild.me.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ctx.message.channel.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True),
+            ctx.message.author: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        }
+        logs = await ctx.message.channel.guild.me.guild.create_text_channel("┌📈𝗟𝗢𝗚𝗦", overwrites=overwrites, category=category)
+        alerts = await ctx.message.channel.guild.me.guild.create_text_channel("└👥𝗔𝗟𝗘𝗥𝗧𝗦", overwrites=overwrites, category=category)
 
-    query = "INSERT INTO tournaments (name, userid, serverid, logchannel, alertchannel) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')".format(
-        escape_string(name), ctx.message.author.id, ctx.message.channel.guild.me.guild.id, logs.id, alerts.id)
-    mycursor.execute(query)
-    con.commit()
-    con.close()
+        av_commands = "Here's the list of available commands that can be used in {0} or {1} by organizers!\n`closeconnection name` - Close the connection for a" \
+                      "user.\n**Example:** `closeconnection iLearner`\n\n`processlist name` - Get the current list of running processes from task manager for a " \
+                      "user.\n**Example:**`processlist iLearner`\n\nHope you enjoy your time!".format(logs.mention,
+                                                                                                      alerts.mention)
+        em = discord.Embed(title='Available commands', description=av_commands, colour=0x2ecc71)
+        em.set_author(name='', icon_url=ctx.message.author.avatar_url)
+        em.set_footer(text=name, icon_url=ctx.message.channel.guild.me.avatar_url)
+        await logs.send(embed=em)
+        await alerts.send(embed=em)
+
+        myemoji = client.get_emoji(787237116630532106)
+        chemoji = client.get_emoji(788129603876421632)
+        msg = '┌─────┈{0}┈────┐\n{6}Tournament created!{7}\n└─────┈{1}┈────┘\n\nTournament has been created!\n\nTournament name: {2}\nTournament organizer: {3}\nLogs channel: {4}\nAlerts channel: {5}\n\nPlease add members to your tournament with the command `itourney_role @role`\n\nSend private keys to all members before the tournament with the command `isendkeys`'.format(
+            myemoji, myemoji, name, ctx.message.author.mention, logs.mention, alerts.mention, chemoji, chemoji)
+        em = discord.Embed(title='', description=msg, colour=0x9b59b6)
+        em.set_author(name='', icon_url=ctx.message.author.avatar_url)
+        em.set_footer(text="Tournament created!", icon_url=ctx.message.channel.guild.me.avatar_url)
+        await ctx.message.channel.send(embed=em)
+
+        query = "INSERT INTO tournaments (name, userid, serverid, logchannel, alertchannel) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')".format(
+            escape_string(name), ctx.message.author.id, ctx.message.channel.guild.me.guild.id, logs.id, alerts.id)
+        mycursor.execute(query)
+        con.commit()
+        con.close()
+    except:
+        sendError("Something went wrong... contant admin!", "", ctx)
--- event/on_message.py
+++ event/on_message.py
@@ -3,10 +3,9 @@ import discord
 import time
 from emoji.emoji import delete_emoji
 from iBot import client, emoji_list, muted_list
-from mute.mute import delMute
+from mute.mute import delMute, isMuted
 from reaction.reaction import getReactionTime, setReactionTime
 from strings.quotes import quotes, answers
-
 
 
 @client.event
@@ -48,7 +47,7 @@ async def handleReaction(message):
         if message.channel.guild.id == x.server and message.author.id == x.user:
             cur_time = int(time.time())
             last_reaction_time = await getReactionTime(message.author.id)
-            if (cur_time - last_reaction_time) > 2:
+            if (cur_time - last_reaction_time) > 2 and not isMuted(message.author.id, message.channel.guild.id):
                 try:
                     emoji = client.get_emoji(x.emoji)
                     await message.add_reaction(emoji)
